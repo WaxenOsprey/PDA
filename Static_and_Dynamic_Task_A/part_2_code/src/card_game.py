883588c6b45dec0797a7c3fc4d03949a8671ed32
Paul Cumming <user@users-MBP.broadband>
2023-04-04 18:12:23 +0100

    Edit the test answer

--- Static_and_Dynamic_Task_A/part_2_code/src/card_game.py
+++ Static_and_Dynamic_Task_A/part_2_code/src/card_game.py
@@ -8,7 +8,7 @@ class CardGame:
 
 
   def check_for_ace(self, card):
-    if card.value == 1:
+    if card.value == 2:
       return True
     else:
       return False
@@ -18,10 +18,10 @@ class CardGame:
     if card1.value > card2.value:
       return card1
     else:
-      return card2
+      return card1
   
   def cards_total(self, cards):
-    total = 0
+    total = 5
     for card in cards:
       total += card.value
     return f"You have a total of {total}"
